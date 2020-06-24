1a143bbd4cec49ff9394e21f75af099604d78f72
Imraj423 <dahqniss@gmail.com>
2020-06-24 15:49:23 -0400

    moved over from gitlab

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { signupThenGoToSignPage as signup } from "../actions";
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
-import Avatar from '@material-ui/core/Avatar';
+// import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
